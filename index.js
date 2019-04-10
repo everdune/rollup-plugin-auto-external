105e855aa6c9cf8d40589de808ae5a29ed434cdd
Robin Hos <robin.hos@gmail.com>
2019-04-10 12:44:25 +0200

    [FIX] support all aws-sdk import cases

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const getBuiltins = require('builtins');
-const path = require('path');
 const readPkg = require('read-pkg');
-const safeResolve = require('safe-resolve');
 const semver = require('semver');
 
 module.exports = ({
@@ -27,8 +25,6 @@ module.exports = ({
       ids = ids.concat(getBuiltins(semver.valid(builtins)));
     }
 
-    ids = ids.map(safeResolve).filter(Boolean);
-
     const external = id => {
       if (typeof opts.external === 'function' && opts.external(id)) {
         return true;
@@ -38,19 +34,7 @@ module.exports = ({
         return true;
       }
 
-      /**
-       * The `id` argument is a resolved path if `rollup-plugin-node-resolve`
-       * and `rollup-plugin-commonjs` are included.
-       */
-      const resolvedPath = safeResolve(id);
-
-      if (resolvedPath === null) {
-        return false;
-      }
-
-      const resolvedDirname = path.dirname(resolvedPath);
-
-      return ids.some(idx => resolvedDirname.startsWith(path.dirname(idx)));
+      return ids.some(idx => id === idx || id.startsWith(idx + '/'));
     };
 
     return Object.assign({}, opts, { external });
